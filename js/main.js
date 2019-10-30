9bbfe25789821f9c367ee64cd6e6a16e5fb127c5
Klaudia Kieszkowska <claudiaandjazz@gmail.com>
2019-10-31 00:18:33 +0100

    JavaScript refactor

--- js/main.js
+++ js/main.js
@@ -1,90 +1,88 @@
-// Create table of letters
+const cyrillic = {
+    // Table of letters: [capital cyrillic, small cyrillic, polish letter]
+    alphabet: [ ["А", "а", "a"], ["Б", "б", "b"], ["В", "в", "w"], ["Г", "г", "g"], ["Д", "д", "d"],
+        ["Е", "е", "je"], ["Ё", "ё", "jo"],["Ж", "ж", "ż"], ["З", "з", "z"], ["И", "и", "i"], ["Й", "й", "j"],
+        ["К", "к", "k"], ["Л", "л", "ł"], ["М", "м", "m"], ["Н", "н", "n"], ["О", "о", "o"], ["П", "п", "p"],
+        ["Р", "р", "r"], ["С", "с", "s"], ["Т", "т", "t"], ["У", "у", "u"], ["Ф", "ф", "f"], ["Х", "х", "ch"],
+        ["Ц", "ц", "c"], ["Ч", "ч", "cz"], ["Ш", "ш", "sz"], ["Щ", "щ", "szcz"], ["Ъ", "ъ", "twardy znak"],
+        ["Ы", "ы", "y"], ["Ь", "ь", "miękki znak"], ["Э", "э", "e"], ["Ю", "ю", "ju"], ["Я", "я", "ja"] ],
 
-var alphabet = [ ["А", "а", "a"], ["Б", "б", "b"], ["В", "в", "w"], ["Г", "г", "g"], ["Д", "д", "d"], 
-                 ["Е", "е", "je"], ["Ё", "ё", "jo"],["Ж", "ж", "ż"], ["З", "з", "z"], ["И", "и", "i"], 
-                 ["Й", "й", "j"], ["К", "к", "k"], ["Л", "л", "ł"], ["М", "м", "m"], ["Н", "н", "n"], 
-                 ["О", "о", "o"], ["П", "п", "p"], ["Р", "р", "r"], ["С", "с", "s"], ["Т", "т", "t"], 
-                 ["У", "у", "u"], ["Ф", "ф", "f"], ["Х", "х", "ch"], ["Ц", "ц", "c"], ["Ч", "ч", "cz"], 
-                 ["Ш", "ш", "sz"], ["Щ", "щ", "szcz"], ["Ъ", "ъ", "twardy znak"], ["Ы", "ы", "y"], 
-                 ["Ь", "ь", "miękki znak"], ["Э", "э", "e"], ["Ю", "ю", "ju"], ["Я", "я", "ja"] ];
+    letterContainer: document.getElementById("letterContainer"),
+    letter: document.getElementById("letter"),
+    keyboard: document.getElementById("alphabet"),
+    keys: document.getElementsByClassName("key"),
+    hintButton: document.getElementById('hint'),
 
+    currentLetter: null,
+    timeout: 1000,
+    initialLetterSize: '6rem',
+    colors: {
+        correct: '#0B8566',
+        wrong: '#A13D32',
+        normal: '#222',
+    },
 
-// Display random letter
+    changeLetter: function () {
+        let num = Math.floor((Math.random() * this.alphabet.length));
+        this.currentLetter = this.alphabet[num];
+        this.letter.innerHTML = this.alphabet[num][0] + this.alphabet[num][1];
+    },
 
-var num;
-var changeLetter = function() {
-    num = Math.floor((Math.random() * alphabet.length));
-    document.getElementById("letter").innerHTML = alphabet[num][0] + alphabet[num][1];
-};
-changeLetter();
+    displayKeyboard: function() {
+        this.alphabet.forEach(letters => {
+            let classes = letters[2].length > 4 ? " smaller" : "";
+            this.keyboard.innerHTML += "<div class='key text-center" + classes + "'>" + letters[2] + "</div>";
+        });
+    },
 
+    handleKeyClick: function(event) {
+        if (event.target.innerText === this.currentLetter[2]) {
+            this.highlightLetter(this.colors.correct);
 
-// Shuffle letter table
+            setTimeout(() => {
+                this.changeLetter();
+                this.highlightLetter();
+            }, this.timeout);
+        } else {
+            this.highlightLetter(this.colors.wrong);
 
-var keyboard = [];
-for (var i = 0; i < alphabet.length; i++) {
-    keyboard.push(alphabet[i][2]);
-}
-for (var i = keyboard.length - 1; i >= 0; i--) {
-    var randomIndex = Math.floor(Math.random() * (i + 1)); 
-    var itemAtIndex = keyboard[randomIndex];
+            setTimeout(() => {
+                this.highlightLetter();
+            }, this.timeout);
+        }
+    },
 
-    keyboard[randomIndex] = keyboard[i]; 
-    keyboard[i] = itemAtIndex;
-}
+    handleHintClick: function() {
+        this.letter.innerHTML = this.currentLetter[2];
+        this.highlightLetter(this.colors.wrong);
 
+        if (this.currentLetter[2].length > 4) {
+            this.letter.style.fontSize = '3rem';
+        }
 
-// Create keyboard with letters
+        setTimeout(() => {
+            this.changeLetter();
+            this.letter.style.fontSize = this.initialLetterSize;
+            this.highlightLetter();
+        }, this.timeout);
+    },
 
-var container = document.getElementById("alphabet");
+    highlightLetter: function(color = this.colors.normal) {
+        this.letterContainer.style.background = color;
+    },
 
-for (var i = 0; i < keyboard.length; i++) {
-    if (keyboard[i].length < 4) {
-        container.innerHTML += "<div class='key text-center'>" + keyboard[i] + "</div>";
-    } else {
-        container.innerHTML += "<div class='key text-center smaller'>" + keyboard[i] + "</div>";
+    init: function () {
+        this.displayKeyboard();
+        this.changeLetter();
+
+        this.letter.style.fontSize = this.initialLetterSize;
+
+        for (let i = 0; i < this.keys.length; i++) {
+            this.keys[i].addEventListener("click", event => this.handleKeyClick(event));
+        }
+
+        this.hintButton.addEventListener("click", () => this.handleHintClick());
     }
-}
-
-
-// Check if clicked letter is correct
-
-var keys = document.getElementsByClassName("key");
-
-for (var i = 0; i < keys.length; i++) {
-    keys[i].addEventListener("click", function() {
-        if (this.innerHTML == alphabet[num][2]) {
-            document.getElementById('letterContainer').style.background = '#0B8566';
-            this.style.background = "#0B8566";
-            var temp = this;
-            setTimeout(function() {
-                changeLetter();
-                document.getElementById('letterContainer').style.background = '#222';
-                temp.style.background = "#222";
-            }, 1000);
-        }
-        else {
-            this.style.background = "#A13D32";
-            var temp = this;
-            setTimeout(function() {
-                temp.style.background = "#222";
-            }, 1000);
-        }
-    }, false);
 };
 
-
-// Button handling
-
-var dontKnow = function() {
-    document.getElementById("letter").innerHTML = alphabet[num][2];
-    // if (alphabet[num][2] == "miękki znak" || alphabet[num][2] == "twardy znak") {
-    //     document.getElementById("letter").style.fontSize = "4rem";
-    // }
-    document.getElementById("letterContainer").style.background = "#A13D32";
-    setTimeout(function() {
-        document.getElementById("letterContainer").style.background = "#222";
-        //document.getElementById("letter").style.fontSize = "6rem";
-        changeLetter();
-    }, 2000);
-}
+cyrillic.init();
